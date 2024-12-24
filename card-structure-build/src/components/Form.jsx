c87a973e016705b964b22ddfb6454f96acf05d46
TChess504 <taderogba5@gmail.com>
2024-12-24 16:59:28 -0600

    still working on form

--- card-structure-build/src/components/Form.jsx
+++ card-structure-build/src/components/Form.jsx
@@ -3,7 +3,7 @@ import {useState} from 'react';
 
 //https://legacy.reactjs.org/docs/forms.html
 function Form() {
-    const [isActive, setIsActive] = useState(true);
+
     let card = new Card();
     const [playerCard, setPlayerCard] = useState(card);
     
@@ -29,96 +29,44 @@ function Form() {
         setPlayerCard(card);
     };
     const handleSubmit = (ev) => {
+        //let errors[];
         ev.preventDefault();
-        //const target = ev.target;
-        //console.log(checkImage('https://example.com/notAnImage.txt'));
-        if(checkImage(playerCard.image)){
-            setIsActive(true);
-            card = new Card(playerCard.name,playerCard.image, playerCard.points, playerCard.rebounds,playerCard.assists, 1);
-            console.log(playerCard.name);
-            setPlayerCard(card);
-            console.log(playerCard);
-        }else{
-            setIsActive(false);
-        }
-    };
-    
-
-    
-    //https://stackoverflow.com/questions/55880196/is-there-a-way-to-easily-check-if-the-image-url-is-valid-or-not
-    
-    async function checkImage(url) {
-        var myHeaders = new Headers();
-        myHeaders.append("Authorization", "Bearer <MY_API_KEY>");
-        myHeaders.append("Cookie", "__cfduid=db290300ecfe95ec1fe3bc92c388c3c991586618117");
-        myHeaders.append("Access-Control-Allow-Origin", "*");
-
-        var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-        let response = fetch(proxyUrl+ url, 
-            { method: "GET" ,
-              headers: myHeaders,
-              
-        });
-        console.log(response.status)
-        /*
-        var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-        let request = new XMLHttpRequest();
-        request.open("GET",proxyUrl+ url, true);
-        request.send();
-        request.onload = function() {
-          if (request.status == 200) //if(statusText == OK)
-          {
-            return true;
-          } else {
-            return false;
-          }
-        }*/
+        card = new Card(playerCard.name,playerCard.image, playerCard.points, playerCard.rebounds,playerCard.assists, 1);
+        setPlayerCard(card);
+        console.log(file);
+        console.log(playerCard);
+        
     }
-
-    /*
-    async function fetchAPI() {
+    
    
-        //let formData = new FormData();
-        //formData.set("priority", 71);
-        let response = await fetch(proxyUrl+ url, 
-          { method: "GET" ,
-            headers: myHeaders,
-            body: new URLSearchParams({
-              'priority': updateValue,
-          })
-        });
-         
-    }
-        */
     //console.log(playerCard);
     return(
         <>
-            <form className = "newPlayerForm" onSubmit = {handleSubmit}>
+            <form className = "newPlayerForm" onSubmit = {handleSubmit} noValidate>
                 <div>
                     <label > Player Name</label>
-                    <input id = "name" name = "name" type = "name" value = {playerCard.name} onChange = {updatePlayerCardName}></input>
+                    <input id = "name" name = "name" type = "name" value = {playerCard.name} onChange = {updatePlayerCardName} requried></input>
                 </div>
                 <div>
-                    <label > Image URL</label>
-                    <input
-                        type="file"
-        name="myImage" value = {playerCard.image} onChange = {updatePlayerCardImageURL}></input>
+                    <label > Image </label>
+                    <input id = "image" name = "image" type="name" value = {playerCard.image} onChange = {updatePlayerCardImageURL} requried></input>
                 </div>
                 <div>
                     <label > Points</label>
-                    <input id = "points" name = "points" type = "number"value = {playerCard.points} onChange = {updatePlayerCardPoints}></input>
+                    <input id = "points" name = "points" type = "number"value = {playerCard.points} onChange = {updatePlayerCardPoints} requried></input>
                 </div>
                 <div>
                     <label > Rebounds</label>
-                    <input id = "rebounds" name = "rebounds" type = "number"value = {playerCard.rebounds} onChange = {updatePlayerCardRebounds}></input>
+                    <input id = "rebounds" name = "rebounds" type = "number"value = {playerCard.rebounds} onChange = {updatePlayerCardRebounds} requried></input>
                 </div>
                 <div>
                     <label > Assists</label>
-                    <input id = "assists" name = "assists" type = "number"value = {playerCard.assists} onChange = {updatePlayerCardAssists}></input>
+                    <input id = "assists" name = "assists" type = "number"value = {playerCard.assists} onChange = {updatePlayerCardAssists} requried></input>
                 </div>
                 <button type = "submit">Add Card</button>
             </form>
-            <span className = {`ImageError  ${isActive ? "show" : "hide"}`} >Image url is not correct please try again</span>
+           
+           
         </>
     )   
 }
