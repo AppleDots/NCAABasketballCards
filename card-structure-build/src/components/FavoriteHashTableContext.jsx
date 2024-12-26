f3afa39343210f67193ea714e2002898a9ba11b2
AppleDots <doty164@gmail.com>
2024-12-26 10:42:15 -0600

    Favorite Added

---
+++ card-structure-build/src/components/FavoriteHashTableContext.jsx
@@ -0,0 +1,22 @@
+import { createContext, useContext, useState } from 'react';
+import {HashTable} from '../features/hashTable.js'
+
+// Create a context
+const HashtableContext = createContext();
+
+// Provide the context
+
+export const HashtableProvider = ({ children }) => {
+  //Create HashTable for Favorite Page(Favorited Players)
+  const [hashtable, setHashtable] = useState(new HashTable((card) => card.charCodeAt(0) * 999 + card.charCodeAt(card.length - 1) *
+  Math.pow(2, card.length - 1)));
+
+  return (
+    <HashtableContext.Provider value={{ hashtable, setHashtable }}>
+      {children}
+    </HashtableContext.Provider>
+  );
+};
+
+// Hook to use the context
+export const useHashtable = () => useContext(HashtableContext);
